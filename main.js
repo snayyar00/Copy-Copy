6eda70508829c9ba07b1adfa7075dbdc534552e3
sidharth Nayyar <sidharth15nayyar@gmail.com>
2019-03-16 16:58:40 -0700

    getting to it
    Change the main content, start working php

--- main.js
+++ main.js
@@ -2,25 +2,25 @@
 $(window).scroll(function() {
 
     if ($(window).scrollTop() > 100) {
-        $('.main_h').addClass('sticky');
+        $('.stickyNav').addClass('sticky');
     } else {
-        $('.main_h').removeClass('sticky');
+        $('.stickyNav').removeClass('sticky');
     }
 });
 
 // Mobile Navigation
 $('.mobile-toggle').click(function() {
-    if ($('.main_h').hasClass('open-nav')) {
-        $('.main_h').removeClass('open-nav');
+    if ($('.stickyNav').hasClass('open-nav')) {
+        $('.stickyNav').removeClass('open-nav');
     } else {
-        $('.main_h').addClass('open-nav');
+        $('.stickyNav').addClass('open-nav');
     }
 });
 
 $('.main_h li a').click(function() {
-    if ($('.main_h').hasClass('open-nav')) {
+    if ($('.stickyNav').hasClass('open-nav')) {
         $('.navigation').removeClass('open-nav');
-        $('.main_h').removeClass('open-nav');
+        $('.stickyNav').removeClass('open-nav');
     }
 });
 
