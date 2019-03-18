18d5f773e52a8bbd12057fd75f5d62e9ccf92ee0
sidharth Nayyar <sidharth15nayyar@gmail.com>
2019-03-18 13:16:36 -0700

    looking good

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ $(window).scroll(function() {
     }
 });
 // If user clicks anywhere outside of the modal, Modal will close
-
-var modal = document.getElementById('modal-wrapper');
-window.onclick = function(event) {
-    if (event.target != modal) {
-        modal.style.display = "none";
-    }
-};
+//
+// var modal = document.getElementById('modal-wrapper');
+// window.onclick = function(event) {
+//     if (event.target != modal) {
+//         modal.style.display="none" ;
+//     }
+// };
 //Pop UP Login
 function popUp(){
 	document.getElementById('modal-wrapper').style.display='block';
