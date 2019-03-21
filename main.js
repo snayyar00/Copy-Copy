05f3f5783cca75dee0af57130931eff8b412caff
sidharth Nayyar <sidharth15nayyar@gmail.com>
2019-03-20 20:34:39 -0700

    cahnges

--- main.js
+++ main.js
@@ -1,51 +1,32 @@
-// Sticky Header
-$(window).scroll(function() {
+$(function(){
 
-    if ($(window).scrollTop() > 100) {
-        $('.stickyNav').addClass('sticky');
-    } else {
-        $('.stickyNav').removeClass('sticky');
-    }
-});
-// If user clicks anywhere outside of the modal, Modal will close
-//
-// var modal = document.getElementById('modal-wrapper');
-// window.onclick = function(event) {
-//     if (event.target != modal) {
-//         modal.style.display="none" ;
-//     }
-// };
-//Pop UP Login
-function popUp(){
-	document.getElementById('modal-wrapper').style.display='block';
-};
-function popUpClose(){
-	document.getElementById('modal-wrapper').style.display='none';
-};
 
-// Mobile Navigation
-$('.mobile-toggle').click(function() {
-    if ($('.stickyNav').hasClass('open-nav')) {
-        $('.stickyNav').removeClass('open-nav');
-    } else {
-        $('.stickyNav').addClass('open-nav');
-    }
+  $("#open-search").on("click",function(){
+    $(".search-form").fadeToggle();
+  $( "#search-bar" ).focus();});
+
+$(".close-button").on("click",function(){
+  $(".search-form").fadeOut();
+$( "#open-search" ).focus();});
+
+
+$(window).on("scroll", function() {
+var scroll = $(window).scrollTop();
+var heightNav = $("header").height();
+//alert(heightNav);
+var heightWrapper=$(".wrapper").height();
+
+if (scroll >= heightNav)  {
+
+    $(".sticky-bar").addClass("decNav");
+
+
+}
+
+else{
+  $(".sticky-bar").removeClass("decNav");
+}
 });
 
-$('.main_h li a').click(function() {
-    if ($('.stickyNav').hasClass('open-nav')) {
-        $('.navigation').removeClass('open-nav');
-        $('.stickyNav').removeClass('open-nav');
-    }
-});
 
-// navigation scroll lijepo radi materem
-$('nav a').click(function(event) {
-    var id = $(this).attr("href");
-    var offset = 70;
-    var target = $(id).offset().top - offset;
-    $('html, body').animate({
-        scrollTop: target
-    }, 500);
-    event.preventDefault();
 });
