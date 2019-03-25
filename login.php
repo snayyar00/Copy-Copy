f59614d48b6d25ca46dc140011d66d616b66ecad
sidharth Nayyar <sidharth15nayyar@gmail.com>
2019-03-25 02:28:38 -0700

    ALMOST

--- login.php
+++ login.php
@@ -1,52 +1,92 @@
-
 <?php
+if($_POST)
+{
+    $conn=mysqli_connect('localhost','root','','CopyPaste');
 
-// Should return a PDO
-require_once 'config.php';
+    $email=$_POST['email'];
+    $password=md5($_POST['password']);
+    $email = stripslashes($email);
+    $password = stripslashes($password);
+    $email = mysql_real_escape_string($email);
+    $password = mysql_real_escape_string($password);
+    $query="SELECT * FROM `members` WHERE email='$email' AND password='$password' ";
+    if(mysqli_query($conn,$query))
+    {
+      session_start();
+      $_SESSION['loggedin'] = true;
+      $_SESSION['email'] = $email;
+      echo"<script>console.log('yo');</script>";
 
-
-  try {
-    // TODO
-    $connectionString = "mysql:host=" . DBHOST . ";dbname=" . DBNAME;
-    $user=DBUSER;
-    $pass=DBPASS;
-    $pdo = new PDO($connectionString,$user,$pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // try to open database connection using constants set in config.php
-     return $pdo;
-  }
-  catch (PDOException $e)
-  {
-    die($e->getMessage());
-  }
-  ;
-
-
-
-   session_start();
-
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form
-      $username = stripslashes($username);
-      $password = stripslashes($password);
-      $username = mysql_real_escape_string($username);
-      $password = mysql_real_escape_string($password);
-      $sql="SELECT * FROM $tbl_name WHERE username='$username' and password='$password'";
-      $result=mysql_query($sql);
-
-      // Mysql_num_row is counting table row
-      $count=mysql_num_rows($result);
-
-      // If result matched $username and $password, table row must be 1 row
-      if($count == 1) {
-         session_register("$username");
-         $_SESSION['login_user'] = $username;
-
-         header("location: indexLogged.php");
-      }
-      else {
-         $error = "Your Login Name or Password is invalid";
-      }
     }
-
+    else
+    {
+        echo 'Error occured!!';
+    }
+    mysqli_close($conn);
+};
 ?>
+<html>
+<head>
+<title>Log In</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script>
+<link href="styleLog.css" type="text/css">
+<script src="main.js"></script>
+  <script type="text/javascript">
+     $(document).ready(function(){
+         // WILL PREVENT FORM FROM SENDING IF THERE ARE ERRORS (comment this out to test PHP validation.)
+       $.validate();
+       });
+    </script>
+    <style>
+    <?php include 'styleLog.css'; ?>
+    </style>
+</head>
+<body >
+  <header class="stickyNav sticky">
+    <a href="#top" class="icon-circle-up"></span>
+      <div class="row">
+        <a class="logo" href="index.html">COPY-Paste</a>
+
+
+
+        <nav>
+          <ul>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="login.php" id="login" onclick="popUp()">Login</a></li>
+            <li><a href="Register.php">Sign Up</a></li>
+          </ul>
+        </nav>
+
+      </div> <!-- / row -->
+
+  </header>
+<div id="Sign-Up">
+  <form  method="POST" action="login.php" >
+    <div class="container">
+      <h1>Log in</h1>
+      <p>Enter your credentials.                   </p>
+      <hr>
+<div>
+      <label for="email" ><b>Email</b></label>
+      <input type="email" id="email" placeholder="Enter Email" name="email" required data-validation="email" >
+</div>
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="password"  required>
+
+
+<div>
+      <label>
+        <input type="checkbox" checked="checked" name="remember" > Remember me
+      </label>
+</div>
+
+
+      <input type="submit" name="sumbit" value="Log in">
+  </div>
+  </div>
+  </form>
+</div>
+</body>
+</html>
